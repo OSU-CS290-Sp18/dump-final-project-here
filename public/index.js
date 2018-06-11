625d849d76395b392ff3139bfc8d84622e78a7db
Brenden Smith <smitbre2@onid.orst.edu>
2018-06-10 22:01:50 -0700

    Added unstylized 5-star review system complete with counter.

--- public/index.js
+++ public/index.js
@@ -1,23 +1,60 @@
-/* Get from mongoDB*/
+/* Get from mongoDB */
 var counter = 0;
+var rating = 0;
 
-var container = document.getElementsByClassName("rank-test");
 
+
+/* Will recieve all rating based information */
+/* Prevents the page from loading like shit */
 window.onload = function() {
-  var rankUp = document.getElementById("rank-up").addEventListener("click", function() {
+
+  /* Catches exceotion for no database information. Will prevent listing of NaN or NULL*/
+  if(rating === 0) {
+    document.getElementById("rank-counter").innerHTML = "No Reviews";
+  }
+
+  /* Following code blocks will input different rating values */
+  var rankOne = document.getElementById("rank-1").addEventListener("click", function() {
     /* Will have to be sent back to Mongo */
     counter++;
-
-    console.log("== Count incremented. Is currently: ", counter);
+    rating += 1;
+    updateCounter();
   });
 
-
-  var rankDown = document.getElementById("rank-down").addEventListener("click", function() {
+  var rankTwo = document.getElementById("rank-2").addEventListener("click", function() {
     /* Will have to be sent back to Mongo */
-    counter--;
+    counter++;
+    rating += 2;
+    updateCounter();
+  });
 
-    console.log("== Count decremented. Is currently: ", counter);
+  var rankThree = document.getElementById("rank-3").addEventListener("click", function() {
+    /* Will have to be sent back to Mongo */
+    counter++;
+    rating += 3;
+    updateCounter();
+
+  })
+  var rankFour = document.getElementById("rank-4").addEventListener("click", function() {
+    /* Will have to be sent back to Mongo */
+    counter++;
+    rating += 4;
+    updateCounter();
 
   });
 
+  var rankFive = document.getElementById("rank-5").addEventListener("click", function() {
+    /* Will have to be sent back to Mongo */
+    counter++;
+    rating += 5;
+    updateCounter();
+
+  });
+}
+
+function updateCounter() {
+  var newRating = rating / counter;
+  /* Enforces 2 decimal places */
+  newRating = newRating.toFixed(2);
+  document.getElementById("rank-counter").innerHTML = newRating;
 }
