88f9557715f696ac0eaf6854f3fc1c29a83a56b2
Grayland Lunn <lunng@oregonstate.edu>
2018-06-12 02:37:55 -0700

    serving is wroking as expected, no errors

--- server.js
+++ server.js
@@ -16,21 +16,30 @@ var port = process.env.PORT || 3040;
 app.engine('handlebars',exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/here', function(req, res, next){
+app.get('/', function(req, res, next){
 	res.status(200).render('parks_page', {
 		parks: parksData
 	});
 });
 
 app.get('/parks/:park', function (req, res, next) {
-  var park = req.params.park.toLowerCase();
-  if (parksData[park]) {
-    res.status(200).render('parks_page', parksData[park]);
-  } else {
+  var park_input = req.params.park.toLowerCase();
+  console.log("park_input == ", park_input);
+  console.log("parksData.park_input ==", parksData.park_input);
+  console.log("parksData ==", parksData);
+  if(parksData[park_input]){
+    res.status(200).render('parks_page', {
+	parks: [parksData[park_input]] });
+  }
+  else{
     next();
   }
+});
+
+app.get('*', function (req, res, next) {
+  res.status(404).render('404_page');
 });
 
 app.listen(port, function () {
