225ee80b2fa45c86b8cda8d96a68436a370cc1f4
Brenden Smith <smitbre2@flip3.engr.oregonstate.edu>
2018-06-11 23:57:42 -0700

    Added mongoDB integration. Has trouble reading from parks collection

--- server.js
+++ server.js
@@ -7,16 +7,33 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST || "classmongo.engr.oregonstate.edu";
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUsername = process.env.MONGO_USERNAME || "cs290_smitbre2";
+var mongoPassword = process.env.MONGO_PASSWORD || "cs290_smitbre2";
+var mongoDBName = process.env.MONGO_DB_NAME || "cs290_smitbre2";
+
+var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
+
+var mongoDB = null; 
 
 var parksData = require('./parks');
 
 var app = express();
-var port = process.env.PORT || 3040;
+var port = process.env.PORT || 27017;
 
 app.engine('handlebars',exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.get('/', function(req, res, next) {
+	res.status(200).render('index.html')
+})
 
 app.get('/here', function(req, res, next){
 	res.status(200).render('parks_page', {
@@ -24,15 +41,29 @@ app.get('/here', function(req, res, next){
 	});
 });
 
+
 app.get('/parks/:park', function (req, res, next) {
-  var park = req.params.park.toLowerCase();
-  if (parksData[park]) {
-    res.status(200).render('parks_page', parksData[park]);
-  } else {
-    next();
-  }
+  var park = require.params.park.toLowerCase(); 
+  var parksCollection = mongoDB.collection('parks');
+
+  parksCollection.find({id: park}).toArray(function(err, parksDocs) {
+  	if (err) {
+		res.status(500).send("Error fetching park from DB.");
+	}
+	else {
+		res.status(200).render('parks_page', parksDocs[0]);
+	}
+  })
 });
 
-app.listen(port, function () {
-  console.log("== Server is listening on port rendering the proper server", port);
-});
+
+MongoClient.connect(mongoURL, function (err, client) {
+	if(err) {
+		throw err;
+	}
+
+	mongoDB = client.db(mongoDBName);
+	app.listen(port, function () {
+	  	console.log("== Server is listening on port rendering the proper server", port);
+	});
+})
